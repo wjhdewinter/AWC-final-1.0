aeaf36026a92bb9af9fe0cfbea30328b86e72d51
wjhdewinter <wjhdewinter@gmail.com>
2026-03-28 07:23:00 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("awc-pallets").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js"
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
