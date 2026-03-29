f1dc9d3003de50b98c80b171a1686da2328bed61
wjhdewinter <wjhdewinter@gmail.com>
2026-03-29 15:17:45 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,30 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", e => self.clients.claim());
+const CACHE_NAME = "awc-v2";
+
+const urlsToCache = [
+  "/Awcv1/",
+  "/Awcv1/index.html",
+  "/Awcv1/manifest.json",
+  "/Awcv1/icon-192.png",
+  "/Awcv1/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
